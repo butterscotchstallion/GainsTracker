eac8007c10fec402831ee2c4a5cd0881ed84e25c
Butterscotch Stallion <swift.plan5016@fastmail.com>
2024-11-22 00:05:25 -0500

    Adds program URLs/views

--- programs/views.py
+++ programs/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.views import generic
+
+
+class IndexView(generic.ListView):
+    template_name = "index.html"
+
+
+class DetailView(generic.DetailView):
+    template_name = "details.html"
