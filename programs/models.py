8cabe43aedd46f56de22a734d5c8a320e0a7adbb
Butterscotch Stallion <swift.plan5016@fastmail.com>
2024-11-23 11:13:02 -0500

    Adds program model/template

--- programs/models.py
+++ programs/models.py
@@ -1,1 +1,9 @@
-# Create your models here.
+from django.db import models
+
+
+class Program(models.Model):
+    program_name = models.CharField(max_length=50)
+    pub_date = models.DateTimeField("date published")
+
+    def __str__(self):
+        return self.program_name
--- programs/views.py
+++ programs/views.py
@@ -1,9 +1,20 @@
 # Create your views here.
+
 from django.views import generic
+
+from programs.models import Program
 
 
 class IndexView(generic.ListView):
     template_name = "index.html"
+    context_object_name = "programs_list"
+
+    def get_queryset(self):
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Program.objects.all()
 
 
 class DetailView(generic.DetailView):
