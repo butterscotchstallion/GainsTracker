fa76b510237d3f07728c47667f8778e1304e06f0
Butterscotch Stallion <swift.plan5016@fastmail.com>
2024-11-25 18:49:17 -0500

    Refactors to use DRF views

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,36 @@
-from django.urls import path
+from django.core.handlers.wsgi import WSGIRequest
+from django.urls import include, path
+from rest_framework import routers, serializers, viewsets
+from rest_framework.response import Response
 
-from .views import program_list
+from programs.models import Program
+
+
+# Serializers define the API representation.
+class ProgramSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Program
+        fields = ["program_name", "pub_date"]
+
+
+# ViewSets define the view behavior.
+class ProgramViewSet(viewsets.ModelViewSet):
+    queryset = Program.objects.all()
+    serializer_class = ProgramSerializer
+
+    def get(self, request: WSGIRequest) -> Response:
+
+        return Response(self.serializer_class.data)
+
+    def post(self):
+        pass
+
+
+router = routers.DefaultRouter()
+router.register(r"programs", ProgramViewSet)
 
 urlpatterns = [
-    path("programs/", program_list, name="program_list"),
+    # path("programs/", program_list, name="program_list"),
+    path("", include(router.urls)),
+    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
 ]
--- api/views.py
+++ api/views.py
@@ -1,9 +1,23 @@
-from django.http import JsonResponse
+from typing import Type
+
+from django.core import serializers
+from django.core.handlers.wsgi import WSGIRequest
+from django.http import Http404, JsonResponse
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 
 from programs import models
+from programs.models import Program
 
 
 @api_view(["GET"])
-def program_list(request):
+def program_list(request: WSGIRequest) -> JsonResponse:
     return JsonResponse({"programs": list(models.Program.objects.all().values())})
+
+
+@api_view(["GET"])
+def program_detail(
+    request: WSGIRequest, program_id: int
+) -> Type[Http404] | JsonResponse:
+    program = get_object_or_404(Program, id=program_id)
+    return JsonResponse({"program": serializers.serialize("json", program)})
