b3b67e8e876465f2220b5d66c8b08ba065ba6288
Butterscotch Stallion <swift.plan5016@fastmail.com>
2024-12-08 16:32:59 -0500

    Adds tailwind utils, neobrutalism components

---
+++ ui/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Alert from "./lib/components/alert.tsx";
+
+function App() {
+    return (
+        <>
+            <Alert message={"Gains Tracker"}/>
+        </>
+    )
+}
+
+export default App
