b0b2cb83d5f382bfaf3344d489ff8c44f1e8fb70
Butterscotch Stallion <swift.plan5016@fastmail.com>
2024-12-08 17:20:05 -0500

    Adds bgcolor/text color

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -3,10 +3,10 @@ import './App.css'
 function App() {
     return (
         <>
-            <div className="flex items-stretch bg-grey-lighter min-h-screen">
+            <div className="flex items-stretch bg-darkBg w-full m-0 min-h-screen">
                 <header>
                     <div className="w-32 content-center inline-block">
-                        <h1>
+                        <h1 className="text-slate-100">
                             Gains
                             <br/>
                             Tracker
