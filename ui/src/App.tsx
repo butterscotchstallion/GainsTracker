2b30f0043790b81cec9d1b3f3cab62a5a6a0cb50
Butterscotch Stallion <swift.plan5016@fastmail.com>
2024-12-13 23:46:56 -0500

    Adjusts menu

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -14,10 +14,14 @@ function App() {
                 </header>
             </div>
             <main className="p-2 bg-slate-300 w-full h-screen">
-                <menu className="p-3 w-32 bg-slate-500 border-solid border-black border-2">
-                    <ul className="list-none">
-                        <li><a href="/programs" title="Program list">Programs</a></li>
-                    </ul>
+                <menu className="w-40 bg-slate-500 border-solid border-black border-2">
+                    <div className="grid grid-cols-1 divide-y">
+                        <div className="p-3"><a href="/sessions" title="Sessions">Sessions</a></div>
+                        <div className="p-3"><a href="/sessions" title="Sessions">Programs</a></div>
+                        <div className="p-3"><a href="/test" title="Test">Lorem</a></div>
+                        <div className="p-3"><a href="/test" title="Test">Ipsum Doler</a></div>
+                        <div className="p-3"><a href="/test" title="Test">Amit A Sandwich</a></div>
+                    </div>
                 </menu>
             </main>
         </>
