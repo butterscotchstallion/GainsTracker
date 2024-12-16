5ff32549ba70e32e3f549e430351ef7c2580aeec
Butterscotch Stallion <swift.plan5016@fastmail.com>
2024-12-15 22:52:00 -0500

    Adds api file

---
+++ ui/src/lib/components/api/api.ts
@@ -0,0 +1,16 @@
+import {Configuration, Program, ProgramsApiFactory} from "./generated";
+import {AxiosPromise, RawAxiosRequestConfig} from "axios";
+
+const configuration = new Configuration({
+    basePath: "http://127.0.0.1:8000/api",
+});
+
+export let programsAPI: {
+    programsCreate(data: Program, options?: RawAxiosRequestConfig): AxiosPromise<Program>;
+    programsDelete(id: number, options?: RawAxiosRequestConfig): AxiosPromise<void>;
+    programsList(options?: RawAxiosRequestConfig): AxiosPromise<Array<Program>>;
+    programsPartialUpdate(id: number, data: Program, options?: RawAxiosRequestConfig): AxiosPromise<Program>;
+    programsRead(id: number, options?: RawAxiosRequestConfig): AxiosPromise<Program>;
+    programsUpdate(id: number, data: Program, options?: RawAxiosRequestConfig): AxiosPromise<Program>
+};
+programsAPI = ProgramsApiFactory(configuration);
