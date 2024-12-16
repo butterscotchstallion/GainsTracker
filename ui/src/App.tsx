c1ed5683155f4a3f36a875dfc3c657ebd2c41019
Butterscotch Stallion <swift.plan5016@fastmail.com>
2024-12-15 19:24:01 -0500

    Fixes routing

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,49 +1,13 @@
 import './App.css'
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faDumbbell, faRectangleList} from "@fortawesome/free-solid-svg-icons";
-import Button from "./lib/components/button.tsx";
-import {NavLink, Outlet} from "react-router";
+import BaseLayout from "./lib/routes/BaseLayout.tsx";
+import {Outlet} from "react-router";
 
 function App() {
     return (
         <>
-            <div className="flex items-stretch bg-darkBg w-full">
-                <header className="w-full p-4 bg-headerBg">
-                    <div className="w-32 content-center inline-block">
-                        <NavLink to="/" end>
-                            <img src="images/logo.png" className="border-0" alt="Gains Tracker" width="250"
-                                 height="150"/>
-                        </NavLink>
-                    </div>
-                </header>
-            </div>
-
-            <main className="bg-slate-300 w-full h-screen">
-                <div className="flex">
-                    <menu className="flex-none h-screen">
-                        <div className="p-4">
-                            <NavLink to="/sessions" end>
-                                <Button>
-                                    <FontAwesomeIcon icon={faDumbbell}/>&nbsp; Sessions
-                                </Button>
-                            </NavLink>
-                        </div>
-                        <div className="p-4">
-                            <NavLink to="/programs" end>
-                                <Button>
-                                    <FontAwesomeIcon icon={faRectangleList}/>&nbsp;  Programs
-                                </Button>
-                            </NavLink>
-                        </div>
-                    </menu>
-
-                    <div className="pt-1 pl-3 pr-4 pb-4 grow">
-                        <article className="text-pretty flow-text break-words">
-                            <Outlet/>
-                        </article>
-                    </div>
-                </div>
-            </main>
+            <BaseLayout>
+                <Outlet/>
+            </BaseLayout>
         </>
     )
 }
--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,20 +1,24 @@
-import {StrictMode} from 'react'
+import {StrictMode, Suspense} from 'react'
 import {createRoot} from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import {BrowserRouter, Route, Routes} from 'react-router'
-import ProgramsPage from "./lib/pages/programs.tsx";
-import SessionsPage from "./lib/pages/sessions.tsx";
+import ProgramsPage from "./lib/routes/programs.tsx";
+import SessionsPage from "./lib/routes/sessions.tsx";
+import {Loader} from "lucide-react";
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <BrowserRouter>
-            <Routes>
-                <Route index element={<App/>}/>
-                <Route path="sessions" element={<SessionsPage/>}/>
-                <Route path="programs" element={<ProgramsPage/>}/>
-            </Routes>
-        </BrowserRouter>
+        <Suspense fallback={<Loader/>}>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<App/>}>
+                        <Route path="programs" element={<ProgramsPage/>}/>
+                        <Route path="sessions" element={<SessionsPage/>}/>
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </Suspense>
     </StrictMode>,
 )
 
