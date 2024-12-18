1c665605175d7e19c6f4083d01850d76ecf2c266
Butterscotch Stallion <swift.plan5016@fastmail.com>
2024-12-17 23:14:52 -0500

    Adds calendar

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -6,6 +6,7 @@ import {BrowserRouter, Route, Routes} from 'react-router'
 import ProgramsPage from "./lib/routes/programs.tsx";
 import SessionsPage from "./lib/routes/sessions.tsx";
 import {Loader} from "lucide-react";
+import SchedulePage from "./lib/routes/schedule.tsx";
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
@@ -13,6 +14,7 @@ createRoot(document.getElementById('root')!).render(
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<App/>}>
+                        <Route path="schedule" element={<SchedulePage/>}/>
                         <Route path="programs" element={<ProgramsPage/>}/>
                         <Route path="sessions" element={<SessionsPage/>}/>
                     </Route>
