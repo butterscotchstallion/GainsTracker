d1eb6358f6a6b71de0dd7ef0c76fa1b5444e1bb2
Butterscotch Stallion <swift.plan5016@fastmail.com>
2024-12-24 23:42:35 -0500

    Removes old logo, moves styles into own directory

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,4 +1,4 @@
-import './App.css'
+import './styles/App.css'
 import BaseLayout from "./lib/routes/BaseLayout.tsx";
 import {Outlet} from "react-router";
 import {changeTheme} from "./lib/components/themeSwitcher.ts";
@@ -6,7 +6,7 @@ import {changeTheme} from "./lib/components/themeSwitcher.ts";
 function App() {
     const themeName = localStorage.getItem('theme') || 'plum';
     changeTheme(themeName);
-    
+
     return (
         <>
             <BaseLayout>
--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,6 +1,6 @@
 import React, {Suspense} from 'react'
 import {createRoot} from 'react-dom/client'
-import './index.scss'
+import './styles/index.scss'
 import App from './App.tsx'
 import {BrowserRouter, Navigate, Route, Routes} from 'react-router'
 import Throbber from "./lib/components/Throbber.tsx";
